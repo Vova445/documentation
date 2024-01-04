1b390a35e5a1312002d9e190791d425d151a5fdd
Volodya Mikhnov <vvvmikhnov@gmail.com>
2024-01-04 03:27:00 +0200

    newsSection

--- src/newsAPI.js
+++ src/newsAPI.js
@@ -1,13 +1,34 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3001/api/news';
+const API_KEY = '47be95dde8bc440ab4c16802154c1952';
+const API_URL = 'https://newsapi.org/v2/everything';
 
 export const fetchProgrammingNews = async () => {
   try {
-    const response = await axios.get(API_URL);
-    return response.data;
+    const oneWeekAgo = new Date();
+    oneWeekAgo.setDate(oneWeekAgo.getDate() - 7);
+
+    const response = await axios.get(API_URL, {
+      params: {
+        q: 'programming',
+        apiKey: API_KEY,
+        from: oneWeekAgo.toISOString().split('T')[0], 
+      },
+    });
+
+    const programmingNews = response.data.articles;
+    return programmingNews;
   } catch (error) {
     console.error('Error fetching programming news:', error);
+    if (error.response) {
+      console.error('Response data:', error.response.data);
+      console.error('Response status:', error.response.status);
+      console.error('Response headers:', error.response.headers);
+    } else if (error.request) {
+      console.error('No response received. Request:', error.request);
+    } else {
+      console.error('Error setting up the request:', error.message);
+    }
     return [];
   }
 };
