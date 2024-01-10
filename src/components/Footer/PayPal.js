480b2d6a5d42bd143c2e46279f24d56b4d649534
Volodya Mikhnov <vvvmikhnov@gmail.com>
2024-01-10 05:22:02 +0200

    resources

--- src/components/Footer/PayPal.js
+++ src/components/Footer/PayPal.js
@@ -1,16 +1,39 @@
-// paypalApi.js
 import axios from 'axios';
 
-const paypalApi = axios.create({
-  baseURL: 'https://sandbox.paypal.com',
-  headers: {
-    'Content-Type': 'application/json',
-    'Authorization': 'Bearer AUnaSqE4MHRybTEg_BaTy852k6SMj8xK3BsQTxtzoOsx5Ll5n-Dfj4THFlVPRBqWApTXu9Lx-EvA6U8c', 
-  },
-});
+const getAccessToken = async () => {
+  try {
+    const response = await axios.post(
+      'https://api.paypal.com/v1/oauth2/token',
+      null,
+      {
+        auth: {
+          username: 'AUnaSqE4MHRybTEg_BaTy852k6SMj8xK3BsQTxtzoOsx5Ll5n-Dfj4THFlVPRBqWApTXu9Lx-EvA6U8c',
+          password: 'EEaMIJTIz-tvsC5tCS8FqkBXXOI6ZymFPxWtja1mhh4URf33dutk4dZCRkePhPOgV28A5QBqdnr2PX-W',
+        },
+        params: {
+          grant_type: 'client_credentials',
+        },
+      }
+    );
+
+    return response.data.access_token;
+  } catch (error) {
+    throw error.response.data;
+  }
+};
 
 export const createPayment = async (amount) => {
   try {
+    const accessToken = await getAccessToken();
+
+    const paypalApi = axios.create({
+      baseURL: 'https://api.paypal.com',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${accessToken}`,
+      },
+    });
+
     const response = await paypalApi.post('/v1/payments/payment', {
       intent: 'sale',
       payer: {
@@ -19,7 +42,7 @@ export const createPayment = async (amount) => {
       transactions: [
         {
           amount: {
-            total: amount.toString(), 
+            total: amount.toString(),
             currency: 'USD',
           },
         },
